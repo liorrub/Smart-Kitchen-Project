180e2d31e05b6211e903ede28f31b249acb0c765
liorrub <liorrub@post.bgu.ac.il>
2026-06-28 12:10:22 +0300

    Rename frontend directory and remove legacy backend files
    Co-Authored-By: liorrub <lior.rubinshtein2@gmail.com>

--- smart-kitchen-backend/data/Enums/aiEnums.js
+++
@@ -1,9 +0,0 @@
-const aiRequestTypes = [
-    "recipe_generation",
-    "smart_suggestion",
-    "image_analysis"
-];
-
-module.exports = {
-    aiRequestTypes
-};
--- smart-kitchen-backend/src/routes/optionsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const optionsController = require("../controllers/optionsController");
-
-router.get("/", optionsController.getOptions);
-
-module.exports = router;
